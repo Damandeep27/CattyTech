120379b9ca76ff7ad85102100742474bb147ba73
Daman <71846343+Damandeep27@users.noreply.github.com>
2022-09-01 11:25:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ connected
     const server = app.listen(app.settings.port, ()=>console.log("Listening"));
 });
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://catty-tech.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://catty-tech.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
